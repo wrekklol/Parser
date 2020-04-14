d93756d58dabb683d81b5d294378461bd754463f
Bastian <bastian.joergensen321@gmail.com>
2020-04-14 13:55:57 +0200

    .

--- Tools/zipper.ps1
+++ Tools/zipper.ps1
@@ -3,11 +3,9 @@ $targetDir = "C:\Users\Lars\source\repos\Parser\bin\Release\Parser\"#$args[1]
 $targetPath = "C:\Users\Lars\source\repos\Parser\bin\Release\Parser\Parser.dll"#$args[2]
 
 $buildPath = $projectDir + "Builds\zipped.zip"
-$7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
+$7zipPath = "C:\Program Files\7-Zip\7z.exe"
 
-
-
-if (-not (Test-Path -Path $7zipPath -PathType Leaf)) 
+if (-not (Test-Path -Path $7zipPath -PathType Leaf))
 {
     throw "7 zip file '$7zipPath' not found"
 }
@@ -17,11 +15,19 @@ Set-Alias 7zip $7zipPath
 $Source = $targetDir
 $Target = $buildPath
 
-7zip a -y -tzip $Target $Source -mx=9
+7zip a -y -tzip $Target $Source -mx=0
 
 
 
 $a  = [System.Reflection.Assembly]::LoadFrom($targetPath);
 $p = "Parser-v" + $a.GetName().Version.ToString() + ".zip";
+$fullPath = $projectDir + "Builds\" + $p
+
+if (Test-Path -Path $fullPath -PathType Leaf)
+{
+    Remove-Item -Path $fullPath
+}
 
 Rename-Item $buildPath $p
+
+Read-Host -Prompt "Press Enter to exit"
