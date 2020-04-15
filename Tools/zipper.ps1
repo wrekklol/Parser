22a9423271309197d6a83fe71ce149bfb9b10582
Bastian <bastian.joergensen321@gmail.com>
2020-04-15 20:44:18 +0200

    .

--- Tools/zipper.ps1
+++ Tools/zipper.ps1
@@ -26,15 +26,65 @@ $Target = $buildPath
 
 
 
-$a  = [System.Reflection.Assembly]::LoadFrom($targetPath);
-$p = "Parser-v" + $a.GetName().Version.ToString() + ".zip";
-$fullPath = $projectDir + "Builds\" + $p
+$releaseVersion = "v" + [System.Reflection.Assembly]::LoadFrom($targetPath).GetName().Version.ToString()
+$fileName = "Parser-" + $releaseVersion + ".zip"
+$fullPath = $projectDir + "Builds\" + $fileName
 
 if (Test-Path -Path $fullPath -PathType Leaf)
 {
     Remove-Item -Path $fullPath
 }
 
-Rename-Item $buildPath $p
+Rename-Item $buildPath $fileName
 
-Read-Host -Prompt "Press Enter to exit"
+
+
+$bodyCreateRelease = 
+@{
+    "tag_name" = $releaseVersion
+    "target_commitish" = "master"
+    "name" = $releaseVersion
+    "body" = "."
+    "draft" = $false
+    "prerelease" = $false
+} | ConvertTo-Json
+
+$responseCreate = try 
+{ 
+    Invoke-WebRequest -Headers @{"Authorization" = "token a4badf0c9bad9207a52b31c0cb17af8236283c8a"} `
+    -Method POST `
+    -Body $bodyCreateRelease `
+    -Uri https://api.github.com/repos/wrekklol/Parser/releases `
+    -ContentType application/json `
+    -UseBasicParsing
+} 
+catch [System.Net.WebException] 
+{ 
+    Write-Verbose "An exception was caught: $($_.Exception.Message)"
+    $_.Exception.Response 
+}
+
+$jsonObj = ConvertFrom-Json $([String]::new($responseCreate.Content))
+$url = ([System.Uri]('https://uploads.github.com/repos/wrekklol/Parser/releases/' + $jsonObj.id + '/assets?name=' + $fileName)).AbsoluteUri
+
+$responseUpload = try 
+{ 
+    Invoke-WebRequest -Headers @{"Authorization" = "token a4badf0c9bad9207a52b31c0cb17af8236283c8a"} -Method POST -InFile $fullPath -Uri $url -ContentType application/zip -UseBasicParsing
+} 
+catch [System.Net.WebException] 
+{ 
+    Write-Verbose "An exception was caught: $($_.Exception.Message)"
+    $_.Exception.Response 
+}
+
+
+
+Add-Type -AssemblyName System.Windows.Forms
+$global:balmsg = New-Object System.Windows.Forms.NotifyIcon
+$notifpath = (Get-Process -id $pid).Path
+$balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($notifpath)
+$balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
+$balmsg.BalloonTipText = $responseUpload.StatusCode.ToString() + " - " + $responseUpload.StatusDescription.ToString() + " " + $fileName
+$balmsg.BalloonTipTitle = "Build Information"
+$balmsg.Visible = $true
+$balmsg.ShowBalloonTip(20000)
