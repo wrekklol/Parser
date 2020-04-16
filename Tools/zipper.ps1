c532af98862ce1cc3b88230822279fbc29be0001
wrekklol <kevin.sucks123@gmail.com>
2020-04-16 13:49:42 +0200

    .

--- Tools/zipper.ps1
+++ Tools/zipper.ps1
@@ -11,38 +11,33 @@ Param
 
 Add-Type -AssemblyName System.Windows.Forms
 
-$buildPath = $projectDir + "Builds\zipped.zip"
+# Create build
+$releaseVersion = "v" + [System.Reflection.Assembly]::LoadFrom($targetPath).GetName().Version.ToString()
+$fileName = "Parser-" + $releaseVersion + ".zip"
+$buildPath = $projectDir + "Builds\" + $fileName
+$buildFromPath = ($targetDir + "\*")
 $7zipPath = "C:\Program Files\7-Zip\7z.exe"
 
+if (Test-Path -Path $buildPath -PathType Leaf)
+{
+    Remove-Item -Path $buildPath
+}
 if (-not (Test-Path -Path $7zipPath -PathType Leaf))
 {
     throw "7 zip file '$7zipPath' not found"
 }
-
-Set-Alias 7zip $7zipPath
-
-$Source = $targetDir + "\*"
-$Target = $buildPath
-
-7zip a -y -tzip $Target $Source -mx=0
-
-
-
-$releaseVersion = "v" + [System.Reflection.Assembly]::LoadFrom($targetPath).GetName().Version.ToString()
-$fileName = "Parser-" + $releaseVersion + ".zip"
-$fullPath = $projectDir + "Builds\" + $fileName
-
-if (Test-Path -Path $fullPath -PathType Leaf)
+if (-not (Test-Path -Path $buildFromPath -PathType Leaf))
 {
-    Remove-Item -Path $fullPath
+    throw "build '$buildFromPath' not found"
 }
 
-Rename-Item $buildPath $fileName
+Set-Alias 7zip $7zipPath
+7zip a -y -tzip $buildPath $buildFromPath -mx=0
 
 
 
+# Create release
 $usertoken = "token " + (Get-Content -Path ".\Tools\usertoken.txt").Trim()
-
 $bodyCreateRelease = 
 @{
     "tag_name" = $releaseVersion
@@ -73,32 +68,31 @@ $notifpath = (Get-Process -id $pid).Path
 $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($notifpath)
 $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
 $balmsg.BalloonTipText = $responseCreate.StatusCode.ToString() + " - " + $responseCreate.StatusDescription.ToString() + " " + $fileName
-$balmsg.BalloonTipTitle = "Build Create Information"
+$balmsg.BalloonTipTitle = "Release Create Info"
 $balmsg.Visible = $true
 $balmsg.ShowBalloonTip(20000)
 
 
 
-$jsonObj = ConvertFrom-Json $([String]::new($responseCreate.Content))
+# Upload release
+$jsonObj = ConvertFrom-Json $($responseCreate.Content.ToString())
 $url = ([System.Uri]('https://uploads.github.com/repos/wrekklol/Parser/releases/' + $jsonObj.id + '/assets?name=' + $fileName)).AbsoluteUri
 
 $responseUpload = try 
-{ 
-    Invoke-WebRequest -Headers @{"Authorization" = $usertoken} -Method POST -InFile $fullPath -Uri $url -ContentType application/zip -UseBasicParsing
-} 
+{
+    Invoke-WebRequest -Headers @{"Authorization" = $usertoken} -Method POST -InFile $buildPath -Uri $url -ContentType application/zip -UseBasicParsing
+}
 catch [System.Net.WebException] 
-{ 
+{
     Write-Verbose "An exception was caught: $($_.Exception.Message)"
     $_.Exception.Response 
 }
-
-
 
 $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
 $notifpath = (Get-Process -id $pid).Path
 $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($notifpath)
 $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
 $balmsg.BalloonTipText = $responseUpload.StatusCode.ToString() + " - " + $responseUpload.StatusDescription.ToString() + " " + $fileName
-$balmsg.BalloonTipTitle = "Build Upload Information"
+$balmsg.BalloonTipTitle = "Release Upload Info"
 $balmsg.Visible = $true
 $balmsg.ShowBalloonTip(20000)
